fc26035ef7bc71dbb1518987ffd617ad13a4dfb9
Rich Harris <rich.harris@vercel.com>
2023-11-07 13:34:24 -0500

    oops

--- test/samples/path-config/input/index.js
+++ test/samples/path-config/input/index.js
@@ -1,6 +1,6 @@
 /**
- * @param {import('../lib').Input} input
- * @returns {import('../lib').Output}
+ * @param {import('#lib').Input} input
+ * @returns {import('#lib').Output}
  */
 export function foo(input) {
 	return input * 2;

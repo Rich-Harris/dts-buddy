f6590a0eab20b54a7d0731b50b998eddc73eb4ca
Rich Harris <rich.harris@vercel.com>
2024-04-30 18:34:04 -0400

    support 5.5

---
+++ test/samples/basic/output 5.5/index.d.ts
@@ -0,0 +1,24 @@
+declare module 'basic' {
+	/** A vector with two components */
+	export interface Vector2 {
+		/** The x component */
+		x: number;
+		/** The y component */
+		y: number;
+	}
+	/**
+	 * Add two vectors
+	 * @param a the first vector
+	 * @param b the second vector
+	 * */
+	export function add(a: Vector2, b: Vector2): Vector2;
+}
+
+declare module 'basic/subpackage' {
+	/**
+	 * Multiply two vectors
+	 * */
+	export function multiply(a: import("basic").Vector2, b: import("basic").Vector2): import("basic").Vector2;
+}
+
+//# sourceMappingURL=index.d.ts.map

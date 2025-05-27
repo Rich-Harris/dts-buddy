9090c3cf595ab9429d801946edcd66e92ab627d8
Simon Holthausen <simon.holthausen@vercel.com>
2025-05-27 11:53:48 +0200

    fix: don't treat accessors as references
    get/set on interfaces etc were treated as references, leading to false-positive name deduplications

--- test/samples/no-renames/input/index.ts
+++ test/samples/no-renames/input/index.ts
@@ -2,3 +2,9 @@ export { Y } from './type';
 export { Namespace } from './namespace';
 export type X = true;
 export function error(): void {}
+
+export interface Foo {
+	get bar(): number;
+	set bar(x: number);
+}
+export * from './function';
